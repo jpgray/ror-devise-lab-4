99277d20a8b18a15de1c32c46e81ebb53fb544af
Jon Gray <jgray@spartaglobal.com>
2018-11-06 07:17:44 +0000

    v0.6 improved relationships & logic.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,4 +1,4 @@
 class Author < ApplicationRecord
   belongs_to :user
-  has_many :books
+  has_many :books, dependent: :destroy
 end
--- db/migrate/20181105165025_create_books.rb
+++ db/migrate/20181105165025_create_books.rb
@@ -1,6 +1,8 @@
 class CreateBooks < ActiveRecord::Migration[5.2]
   def change
     create_table :books do |t|
+      t.belongs_to :author, index: true
+      t.belongs_to :user, index: true
       t.string :title
       t.integer :page_count
       t.integer :author_id
